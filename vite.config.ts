0b12edb4e75001a62bdfb052d789f0d987ec0d0f
IFLYTEK\yuchen48 <yuchen48@iflytek.com>
2024-10-30 19:49:43 +0800

    配置更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import json from '@rollup/plugin-json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), json()],
+  plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: 1199
